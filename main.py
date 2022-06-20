e6aa60a042b7650a99dcdc2e1d813d8cc1b7f215
Celine Tran <celinetran7737@gmail.com>
2022-06-20 18:16:30 +0000

    concatenation + type cast

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 print("Hello World") # This prints out Hello World
 print("My name is Celine")
 print("Hello" + " " + "World")
+
+myName = "Brandon" # String
+myBirthMonth = 1 # Integer 
+myBirthDate = "18" # String 
+pi = 3.14 # float
+isHungry = True # Boolean 
+
+print("My name is " + myName)
+print("My birth month is " + str(myBirthMonth))
+
